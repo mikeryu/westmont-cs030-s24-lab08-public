e3c16b21df4b3a7dd35f507383d152e01a03859a
Mike Ryu <mryu@westmont.edu>
2025-03-18 10:18:25 -0700

    Updated for Spring 2025.

--- BinarySearcher.java
+++ BinarySearcher.java
@@ -1,6 +1,6 @@
 /**
- * Westmont College Spring 2024
- * CS 030 Lab 08
+ * Westmont College Spring 2025
+ * CS 030 Lab 09
  *
  * @author Assistant Professor Mike Ryu mryu@westmont.edu
  * @author Boaty McBoatface bmcboatface@westmont.edu TODO: replace this with your info.
